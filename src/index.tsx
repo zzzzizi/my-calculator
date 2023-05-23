3c107d56a2fc9c47de88e20554be11cbf3f61227
Yingji Zheng <niangzizeng@gmail.com>
2023-05-23 15:22:30 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './zzz';
+import App from './app';
 
 import { Provider } from 'react-redux';
 import { store } from './store/store';
